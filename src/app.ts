1e80af9d79c5d3e2b5f3ab48d6346b499c017465
Saptarshi <biswassaptarshi2060@gmail.com>
2024-08-22 19:19:32 +0530

    added chat feature

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,31 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import connectDB from './config/database';
 
 import authRouter from "./routes/authRoute";
+import profileRouter from "./routes/profileRoutes";
+import userRouter from "./routes/userRoutes";
 
 const app = express();
 
-// middlewares
+// Connecting MongoDB
+connectDB(); 
+
+// Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
-// routers
+// Routers
 app.use("/auth", authRouter);
+app.use("/profiles", profileRouter); 
+app.use("/users", userRouter); 
 
 app.get("/:id", (req, res) => {
-	res.send(`Hello ${req.body.param.id}`);
+  res.send(`Hello ${req.params.id}`);
 });
 
+// Start the server
 app.listen(8000, () => {
-	console.log("server running on http://localhost:8080");
+  console.log("Server running on http://localhost:8000");
 });
